58069831f2a54d51a9174213e29d00f8667e4972
Marcus <marcus.henriksson@restory.ai>
2024-02-20 10:26:58 +0100

    init commit

---
+++ src/main/java/org/example/Website.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Website {
+
+    public Database userDatabase = new Database();
+    public User currentUser;
+
+    public boolean logIn(String username, String password) {
+        User user = userDatabase.GetUserByUsername(username);
+        if (user.getPassword().equals(password)) {
+            currentUser = user;
+            System.out.println("Login Successful!");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean changeName(String newName) {
+        if (currentUser != null) {
+            userDatabase.SaveNewUsername(currentUser.getUsername(), newName);
+            currentUser.setUsername(newName);
+            return true;
+        }
+        return false;
+    }
+}
