44bb32fd1f0460d831f7157f4dcfb0ecad55fa6e
Marcus <marcus.henriksson@restory.ai>
2024-02-22 13:28:13 +0100

    Added Publish

--- src/test/java/test/RektangelTest.java
+++ src/test/java/test/RektangelTest.java
@@ -13,7 +13,7 @@ public class RektangelTest {
     @BeforeEach
     public void beforeEach() {
         //Skapa ett obejkt av klassen rektangel
-        rektangel = new Rektangel(10, 4);
+        rektangel = new Rektangel(10, 5);
     }
 
 
